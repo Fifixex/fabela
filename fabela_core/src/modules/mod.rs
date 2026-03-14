2722c255f15d3cb88ca167e0afc1a5d2f81850c7
Bjs <fifix_dev@proton.me>
2026-03-14 15:14:08 -0300

    Registry module (#5)
    * ➕ chore(deps): add winreg crate only for
    
    * ☘ feat: add registry module
    
    * 👽 refactor(perf): improve get_hkey function

--- fabela_core/src/modules/mod.rs
+++ fabela_core/src/modules/mod.rs
@@ -1,9 +1,11 @@
 pub mod console;
+pub mod registry;
 
 use rquickjs::{Ctx, Result};
 
 /// Register all native modules into the QuickJS context.
 pub fn register_all(ctx: &Ctx<'_>) -> Result<()> {
     console::register(ctx)?;
+    registry::register(ctx)?;
     Ok(())
 }
