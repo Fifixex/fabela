6af8d14f11ae8c8c71c388075d81f93158f46686
Fifix <fifix_dev@proton.me>
2026-05-14 12:52:29 +0000

    😭 revert: deprecated module

--- fabela_core/src/modules/mod.rs
+++ fabela_core/src/modules/mod.rs
@@ -2,7 +2,6 @@ pub mod child_process;
 pub mod console;
 pub mod fs;
 pub mod os;
-pub mod registry;
 pub mod sqlite3;
 
 use rquickjs::{Ctx, Result};
@@ -10,7 +9,6 @@ use rquickjs::{Ctx, Result};
 /// Register all native modules into the QuickJS context.
 pub fn register_all<'js>(ctx: &Ctx<'js>) -> Result<()> {
     console::register(ctx)?;
-    registry::register(ctx)?;
     fs::register(ctx)?;
     os::register(ctx)?;
     child_process::register(ctx)?;
